dfc7605732524973a0096e373be92d3bdbb3dd93
JoseEgoAguirre <jose.ego10@gmail.com>
2021-11-06 09:56:30 -0500

    Solar System Basic Structure is Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+    SolarSystem andromeda = new SolarSystem();
+    andromeda.moon=4;
+    andromeda.planet=2;
+    andromeda.Sun=1;
+}
+}
